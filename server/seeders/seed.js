1a166df08b9c786610be2faca824178e4a643038
Sam Teel <sjkteel@hotmail.com>
2023-10-05 18:54:11 -0400

    added locationseeds.js seeding to seed.js file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,14 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Location } = require('../models');
 const userSeeds = require('./userseeds.json');
+const locationSeeds = require('./locationseeds.json');
 
 db.once('open', async () =>{
     try {
         await User.deleteMany({});
-
+        await Location.deleteMany({});
         await User.create(userSeeds);
+        await Location.create(locationSeeds);
     }
  catch (err) {
     console.error(err);
