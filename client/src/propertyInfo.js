72f425fe897d5d7331ca755d54810f1b34b24462
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-10-08 06:32:21 -0400

    rendered property info, added auth for non-auth users

---
+++ client/src/propertyInfo.js
@@ -0,0 +1,43 @@
+import beachHouse from "./assets/images/beachHouse.jpg";
+import santiagoHome from "./assets/images/Santiagohome.jpg";
+import lightHouse from "./assets/images/AucklandLighthouse.jpg";
+import apartment from "./assets/images/SaintPetersburgApartment.jpg";
+import riversideLodge from "./assets/images/DusseldorfRiverLodge.jpg";
+
+export const propertyInfo = [
+  {
+    id: 1,
+    image: beachHouse,
+    location: "Miami Beach House",
+    price: "$400",
+    status: "Rented",
+  },
+  {
+    id: 2,
+    image: apartment,
+    location: "Saint Petersburgs Apartment",
+    price: "$250",
+    status: "Open for Rent",
+  },
+  {
+    id: 3,
+    image: santiagoHome,
+    location: "Santiago Home",
+    price: "$150",
+    status: "Rented",
+  },
+  {
+    id: 4,
+    image: riversideLodge,
+    location: "Düsseldorf Riverside Lodge",
+    price: "$80",
+    status: "Open for Rent",
+  },
+  {
+    id: 5,
+    image: lightHouse,
+    location: "Auckland Lighthouse",
+    price: "$75",
+    status: "Open for Rent",
+  },
+];
